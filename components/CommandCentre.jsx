8df1d166a0b2342d6d9f74891808c0e3c1d53016
danydoliveira1-lang <danydoliveira1@gmail.com>
2026-09-18 03:15:14 +0100

    Restore useEffect import in Command Centre

--- components/CommandCentre.jsx
+++ components/CommandCentre.jsx
@@ -1,6 +1,6 @@
 "use client";
 
-import { useState } from "react";
+import { useEffect, useState,} from "react";
 import { printOperationsRunSheet } from "./printOperationsRunSheet";
 import { printManagementReport } from "./printManagementReport";
 import { printProFormaInvoice } from "./printProFormaInvoice";
